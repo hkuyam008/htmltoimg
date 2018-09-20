57ca982de971e6d1175876e1d8f58243bec21256
Administrator <ray.mayukh@gmail.com>
2018-09-20 09:21:20 +0000

    __pycache__ dir removed & htmltoimg.py modified

--- src/core/htmltoimg.py
+++ src/core/htmltoimg.py
@@ -14,4 +14,8 @@ def html_to_img(input_dir, img_format='jpg'):
         output_file_path = os.path.join(output_file_dir, os.path.splitext(html_file)[0]+ '.' + img_format)
         print("Output JPG File : " + output_file_path)
 
+        """ options = {
+            'disable-smart-width': False
+        }"""
+
         imgkit.from_file(input_file_path, output_file_path)
